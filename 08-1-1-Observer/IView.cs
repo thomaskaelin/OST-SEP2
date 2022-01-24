f6d3af77af8cb2f9b36e94f3e5c10b7134b82239
Thomas Kälin <thomas.kaelin1@ost.ch>
2022-01-24 09:29:04 +0100

    8-1-1: Added starter solution.

---
+++ 08-1-1-Observer/IView.cs
@@ -0,0 +1,7 @@
+﻿namespace Observer
+{
+    public interface IView
+    {
+        public void Update(float temperature, float humidity, float windSpeed);
+    }
+}
---
+++ 08-1-1-Observer/WeatherData.cs
@@ -0,0 +1,25 @@
+﻿namespace Observer
+{
+    public sealed class WeatherData : WeatherAPI
+    {
+        private readonly IView _view1, _view2, _view3;
+
+        public WeatherData()
+        {
+            _view1 = new ConsoleView(1);
+            _view2 = new ConsoleView(2);
+            _view3 = new ConsoleView(3);
+        }
+
+        protected override void MeasurementChanged()
+        {
+            var temperature = GetTemperature();
+            var humidity = GetHumidity();
+            var windSpeed = GetWindSpeed();
+
+            _view1.Update(temperature, humidity, windSpeed);
+            _view2.Update(temperature, humidity, windSpeed);
+            _view3.Update(temperature, humidity, windSpeed);
+        }
+    }
+}

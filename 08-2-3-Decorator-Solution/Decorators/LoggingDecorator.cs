67258b1427da2a7995ae45baa078fdfd931bc833
Thomas Kälin <thomas.kaelin1@ost.ch>
2022-01-25 12:59:34 +0100

    8-2-3: Added possible solution.

---
+++ 08-2-3-Decorator-Solution/Decorators/LoggingDecorator.cs
@@ -0,0 +1,21 @@
+﻿namespace Decorator.Decorators
+{
+    using System;
+
+    public sealed class LoggingDecorator : ICalculator
+    {
+        private readonly ICalculator _decoratedCalculator;
+
+        public LoggingDecorator(ICalculator decoratedCalculator)
+        {
+            _decoratedCalculator = decoratedCalculator;
+        }
+
+        public void Calculate()
+        {
+            Console.WriteLine($"{nameof(LoggingDecorator)}: Before calling the decorated object");
+            _decoratedCalculator.Calculate();
+            Console.WriteLine($"{nameof(LoggingDecorator)}: After calling the decorated object");
+        }
+    }
+}
---
+++ 08-2-3-Decorator-Solution/Decorators/StopWatchDecorator.cs
@@ -0,0 +1,26 @@
+﻿namespace Decorator.Decorators
+{
+    using System;
+    using System.Diagnostics;
+
+    public sealed class StopWatchDecorator : ICalculator
+    {
+        private readonly ICalculator _decoratedCalculator;
+
+        public StopWatchDecorator(ICalculator decoratedCalculator)
+        {
+            _decoratedCalculator = decoratedCalculator;
+        }
+
+        public void Calculate()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            _decoratedCalculator.Calculate();
+
+            stopWatch.Stop();
+            Console.WriteLine($"{nameof(StopWatchDecorator)}: The decorated operation took {stopWatch.Elapsed}");
+        }
+    }
+}

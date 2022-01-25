2f44928cc169c839ba76ab4ebf41e5c28491112b
Thomas Kälin <thomas.kaelin1@ost.ch>
2022-01-25 11:07:20 +0100

    8-2-1: Added possible solution.

---
+++ 08-2-1-SimpleFactory-Solution/Dices/LoadedDice.cs
@@ -0,0 +1,11 @@
+﻿namespace SimpleFactory.Dices
+{
+    public class LoadedDice : IDice
+    {
+        private readonly int _fixedRoll;
+
+        public LoadedDice(int fixedRoll) => _fixedRoll = fixedRoll;
+
+        public int Roll() => _fixedRoll;
+    }
+}
---
+++ 08-2-1-SimpleFactory-Solution/Factories/LoadedDiceFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleFactory.Factories
+{
+    using SimpleFactory.Dices;
+
+    public sealed class LoadedDiceFactory : IDiceFactory
+    {
+        public IDice CreateDice(int pipsOnDice) => new LoadedDice(pipsOnDice);
+    }
+}

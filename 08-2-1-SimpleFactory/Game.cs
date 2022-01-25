db2cf7d229876944668b600b0167af59ab4c545a
Thomas Kälin <thomas.kaelin1@ost.ch>
2022-01-25 10:05:28 +0100

    8-2-1: Added starter solution.

---
+++ 08-2-1-SimpleFactory/Game.cs
@@ -0,0 +1,24 @@
+﻿namespace SimpleFactory
+{
+    using System;
+
+    public sealed class Game
+    {
+        private const int PipsOnDice = 6;
+
+        public void Play()
+        {
+            var dice = new Dice(PipsOnDice);
+
+            var roll1 = dice.Roll();
+            var roll2 = dice.Roll();
+            var sum = roll1 + roll2;
+            var hasWon = sum == 2 * PipsOnDice;
+            var message = hasWon ? "You win!" : "You loose!";
+
+            Print(message);
+        }
+
+        private static void Print(string message) => Console.WriteLine(message);
+    }
+}

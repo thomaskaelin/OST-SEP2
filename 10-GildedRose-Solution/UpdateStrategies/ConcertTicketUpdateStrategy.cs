b9db7143cbdb640771707c237dd120e961d9b807
Thomas Kälin <thomas.kaelin1@ost.ch>
2025-02-03 13:33:42 +0100

    Updates for FS25 - Moved files to new folders (08 -> 09, 09 -> 10) - Updated to .NET 9

---
+++ 10-GildedRose-Solution/UpdateStrategies/ConcertTicketUpdateStrategy.cs
@@ -0,0 +1,33 @@
+﻿namespace GildedRose.UpdateStrategies
+{
+    internal sealed class ConcertTicketUpdateStrategy : UpdateStrategy
+    {
+        internal ConcertTicketUpdateStrategy(Item item) : base(item)
+        {
+        }
+
+        internal override void Update()
+        {
+            DecreaseSellInByOne();
+            
+            switch (GetDaysBeforeExpiring())
+            {
+                case < 0:
+                    ResetQuality();
+                    break;
+
+                case <= 5:
+                    IncreaseQualityBy(3);
+                    break;
+
+                case > 5 and <= 10:
+                    IncreaseQualityBy(2);
+                    break;
+
+                default:
+                    IncreaseQualityBy(1);
+                    break;
+            }
+        }
+    }
+}
